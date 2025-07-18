66011fff29cb01128e0eb69d0ee7b46c9f1e61c6
dulice <duliceellen600@gmail.com>
2025-07-18 16:37:17 +0630

    modify prompt

--- components/ui/Call.tsx
+++ components/ui/Call.tsx
@@ -27,6 +27,7 @@ const Call = ({ userId }: { userId: string }) => {
   const [callStatus, setcallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [conversation, setConversation] = useState<Conversation[]>([]);
   const [lastMessage, setLastMessage] = useState("");
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
 
   useEffect(() => {
     const onCallStart = () => {
@@ -78,6 +79,7 @@ const Call = ({ userId }: { userId: string }) => {
   }, []);
 
   const startCall = () => {
+    setErrorMsg(null);
     vapi.start({
       name: "Doctor",
       firstMessage:
@@ -113,20 +115,29 @@ const Call = ({ userId }: { userId: string }) => {
     vapi.stop();
     toast("Meeting has ended");
     setcallStatus(CallStatus.DISCONNECT);
-    const { data } = await axios.post("/api/ai-feedback", {
-      conversation,
-      userId,
-    });
+    try {
+      const { data } = await axios.post("/api/ai-feedback", {
+        conversation,
+        userId,
+      });
 
-    if (data) {
-      const format = JSON.parse(data.replace("```json", "").replace("```", ""));
-      const feedback = await supabase
-        .from("feedback")
-        .insert([{ ...format, userId }]);
-      if (feedback.error) {
-        return toast.error(feedback.error.message);
+      if (data) {
+        const format = JSON.parse(
+          data.replace("```json", "").replace("```", "")
+        );
+        const feedback = await supabase
+          .from("feedback")
+          .insert([{ ...format, userId }]);
+        if (feedback.error) {
+          return toast.error(feedback.error.message);
+        }
+        router.push("/reports");
       }
-      router.push("/reports");
+    } catch (error) {
+      setErrorMsg(
+        `Error while generate the feedback. Try again. Error:${error}`
+      );
+      setcallStatus(CallStatus.ACTIVE);
     }
   };
 
@@ -138,65 +149,69 @@ const Call = ({ userId }: { userId: string }) => {
     }
   };
 
-  if (callStatus == CallStatus.INACTIVE) {
-    return (
-      <div>
-        <p className="text-center text-blue-600">
-          Connect to the doctor, please wait a moment.
-        </p>
-        <Loading />
-      </div>
-    );
-  }
-
-  if (callStatus == CallStatus.DISCONNECT) {
-    return (
-      <div>
-        <p className="text-center text-blue-600">
-          Generating your report, please wait a moment...
-        </p>
-        <p className="text-center text-blue-600">This may take sometimes...</p>
-        <Loading />
-      </div>
-    );
-  }
-
   const MicIcon = vapi.isMuted() ? <MicOff /> : <Mic />;
-
   return (
-    <div>
-      <p className="font-bold text-3xl text-center ">Consult Session</p>
-      <div>
-        <Card className="w-full my-5">
-          <CardContent className="z-10 h-64  flex justify-center items-center relative">
-            {isSpeaking && (
-              <div className="z-1 absolute top-1/2 left-1/2 -translate-1/2 animate-ping bg-blue-300 w-20 h-20 rounded-full"></div>
-            )}
-            <Image
-              src="/images/doctor.jpg"
-              alt=""
-              width={600}
-              height={600}
-              className="object-cover rounded-full w-16 h-16 z-2"
-            />
-          </CardContent>
-        </Card>
-        <div className="message bg-blue-100 h-8 mb-4 px-4 py-2 rounded-md text-center">
-          {lastMessage}
+    <>
+      {callStatus == CallStatus.INACTIVE ? (
+        <div className="mt-5">
+          <p className="text-center text-blue-600">
+            Connect to the doctor, please wait a moment.
+          </p>
+          <Loading />
         </div>
-      </div>
-      <div className="flex justify-center items-center gap-4">
-        <Button onClick={mute} variant="outline" className="rounded-full">
-          {MicIcon}
-        </Button>
-        <Button
-          onClick={disconnectCall}
-          variant="destructive"
-          className="rounded-full">
-          <Phone />
-        </Button>
-      </div>
-    </div>
+      ) : callStatus == CallStatus.DISCONNECT ? (
+        <div className="mt-5">
+          <p className="text-center text-blue-600">
+            Generating your report, please wait a moment...
+          </p>
+          <p className="text-center text-blue-600">
+            This may take sometimes...
+          </p>
+          <Loading />
+        </div>
+      ) : errorMsg ? (
+        <div className="flex flex-col justify-center items-center bg-red-100 gap-10 rounded-2xl p-5 mt-5">
+          <p className="text-red-600">{errorMsg}</p>
+          <Button onClick={startCall} className="rounded-full bg-green-600">
+            <Phone />
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <p className="font-bold text-3xl text-center ">Consult Session</p>
+          <div>
+            <Card className="w-full my-5">
+              <CardContent className="z-10 h-64  flex justify-center items-center relative">
+                {isSpeaking && (
+                  <div className="z-1 absolute top-1/2 left-1/2 -translate-1/2 animate-ping bg-blue-300 w-20 h-20 rounded-full"></div>
+                )}
+                <Image
+                  src="/images/doctor.jpg"
+                  alt=""
+                  width={600}
+                  height={600}
+                  className="object-cover rounded-full w-16 h-16 z-2"
+                />
+              </CardContent>
+            </Card>
+            <div className="message bg-blue-100 h-8 mb-4 px-4 py-2 rounded-md text-center">
+              {lastMessage}
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <Button onClick={mute} variant="outline" className="rounded-full">
+              {MicIcon}
+            </Button>
+            <Button
+              onClick={disconnectCall}
+              variant="destructive"
+              className="rounded-full">
+              <Phone />
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
